6bd9b12a6d3f90b78c77216f80b655f1c322fe6f
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2021-01-01 15:48:20 +0330

    Add files via upload

---
+++ Task 3/Q1/src/task3-Q1.c
@@ -0,0 +1,27 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.12 Advanced
+Automatic Program Generator
+© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : Task3-Q1
+Version : 1
+Date    : 12/30/2020
+Author  : Amir Ebrahimian
+Company : 
+Comments: 
+
+
+
+*******************************************************/
+
+#include <alcd.h>
+
+void main(void)
+{
+    lcd_init(12); 
+    
+   lcd_putsf("Hello World");
+
+}
